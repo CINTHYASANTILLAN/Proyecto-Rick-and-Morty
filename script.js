2dd43b21b8bab7476690d29504582896da5319be
cinthya santillan <cinthya.santillan11@gmail.com>
2023-09-16 21:14:31 -0600

    actualizado

--- script.js
+++ script.js
@@ -1,41 +1,66 @@
-// Creo contenedor para mostrar todos los personajes
+// Defino una variable global para almacenar todos los generos.
+let currentGenderFilter = "todos";
+
+// Función para filtrar personajes por género.
+const filterCharactersByGender = (gender) => {
+    // Actualizo el género actualmente seleccionado.
+    currentGenderFilter = gender;
+    
+    // Reinicio la página actual a 1.
+    currentPage = 1;
+    
+    // Llamo a la función para obtener y mostrar los personajes con el nuevo filtro.
+    getCharacters(currentPage);
+};
+
+// Obtengo el contenedor principal donde se mostrarán las card de los personajes.
 const container = document.getElementById("container");
 
-// Establezco la página actual en 1 para comenzar a cargar la primera página.
+// Establezco la página actual en 1 para comenzar a cargar la primera página de personajes.
 let currentPage = 1;
 
-// Para los botones de "Anterior" y "Siguiente" en el HTML
+// Para los botones de "Anterior" y "Siguiente" 
 const prevButton = document.getElementById("prevButton");
 const nextButton = document.getElementById("nextButton");
 
-// Agrego evento a los botones de "Anterior" y "Siguiente"
+// Modifico la función getCharacters para incluir el filtro de género.
+const getCharacters = (page) => {
+    // Construyo la URL de la API con el número de página y el filtro de género.
+    let apiUrl = `https://rickandmortyapi.com/api/character/?page=${page}`;
+    
+    if (currentGenderFilter !== "todos") {
+        apiUrl += `&gender=${currentGenderFilter}`;
+    }
+
+    // Realizo una solicitud a la API utilizando la URL.
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => renderCharacters(data.results));
+};
+
+// Modifico los botones del menú para que llamen a la función de filtrado con el género correspondiente.
+document.querySelectorAll(".menu button").forEach(button => {
+    button.addEventListener("click", () => {
+        filterCharactersByGender(button.getAttribute("data-gender"));
+    });
+});
+
+// Cuando se hace clic en el botón "Anterior," disminuyo la página actual y cargo la página anterior.
 prevButton.addEventListener("click", () => {
-    // Si la página actual es mayor que 1, disminuyo y cargo la página anterior.
+    // Verifico si la página actual es mayor que 1 antes de disminuir.
     if (currentPage > 1) {
         currentPage--;
         getCharacters(currentPage);
     }
 });
 
+// Cuando se hace clic en el botón "Siguiente," aumento la página actual y cargo la siguiente página.
 nextButton.addEventListener("click", () => {
-    // Aumento la página actual y cargo la siguiente página.
-    currentPage++;
-    getCharacters(currentPage);
+    currentPage++; // Aumento la página actual.
+    getCharacters(currentPage); // Llamo a la función para obtener y mostrar personajes.
 });
 
-
-// Defino una función para obtener y mostrar los personajes de la página especificada.
-const getCharacters = (page) => {
-    // Realizo una solicitud a la API utilizando el número de página proporcionado.
-    fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
-        .then(res => res.json())
-        .then(data => renderCharacters(data.results)); // Llamo a la función para mostrar los personajes.
-};
-
-// Llamo a la función para obtener y mostrar los personajes de la página actual.
-getCharacters(currentPage);
-
-// Defino una función para los personajes en tarjetas.
+// Función para renderizar las tarjetas de personajes.
 const renderCharacters = characters => {
     // Limpio el contenido del contenedor antes de mostrar los nuevos personajes.
     container.innerHTML = "";
@@ -58,7 +83,7 @@ const renderCharacters = characters => {
     renderPagination();
 };
 
-// Defino una función para los botones de paginación.
+// Función para los botones de paginación.
 const renderPagination = () => {
     // Creo un nuevo elemento 'div' para contener los botones de paginación.
     const pagination = document.createElement("div");
@@ -72,30 +97,45 @@ const renderPagination = () => {
     container.appendChild(pagination);
 };
 
-// Defino una función para mostrar la descripción detallada de un personaje.
+// Función para mostrar la descripción detallada de un personaje.
 const verDescription = characterUrl => {
     // Realizo una solicitud a la URL del personaje para obtener su información detallada.
     fetch(characterUrl)
-        .then(res => res.json())
+        .then(res => res.json()) // Convierto la respuesta en JSON.
         .then(character => {
             // Limpio el contenido del contenedor antes de mostrar la descripción.
             container.innerHTML = "";
 
-            // Creo una tarjeta con la información detallada del personaje.
-            const card = document.createElement("div");
-            card.className = "card";
-            card.innerHTML = 
-            `   <h2>${character.name}</h2>
-                <img src="${character.image}" alt="">
-                <p>${character.status}</p>
-                <p>${character.species}</p>
-                <p>${character.gender}</p>
-                <p>${character.origin.name}</p>
-                <p>${character.location.name}</p>
-                <p>${character.episode.name}</p>
-              <button class="button" onclick="getCharacters(currentPage)">Volver</button>`;
-            // Agrego la tarjeta al contenedor para mostrarla en la página.
-            container.appendChild(card);
-        });
-};
-
+             // Creo una tarjeta con la información detallada del personaje.
+             const card = document.createElement("div");
+             card.className = "card-description";
+ 
+             // Agrego un div para la imagen del personaje.
+             const imageDiv = document.createElement("div");
+             imageDiv.className = "card-image";
+             imageDiv.innerHTML = `<img src="${character.image}" alt="">`;
+ 
+             // Agrego la información detallada del personaje al div de la tarjeta.
+             const infoDiv = document.createElement("div");
+             infoDiv.className = "card-info";
+             infoDiv.innerHTML = `
+                 <h2>${character.name}</h2>
+                 <p>Status: ${character.status}</p>
+                 <p>Especie: ${character.species}</p>
+                 <p>Género: ${character.gender}</p>
+                 <p>Origen: ${character.origin.name}</p>
+                 <p>Ubicación: ${character.location.name}</p>
+                 <p>Episodio: ${character.episode[0]}</p>
+                 <button class="button" onclick="getCharacters(currentPage)">Volver</button>
+             `;
+ 
+             // Agrego el div de la imagen y el div de la información a la tarjeta.
+             card.appendChild(imageDiv);
+             card.appendChild(infoDiv);
+ 
+             // Agrego la tarjeta al contenedor para mostrarla en la página.
+             container.appendChild(card);
+         });
+ };
+// Llamo a la función para obtener y mostrar los personajes de la página actual (primero la página 1 con el filtro todos)
+getCharacters(currentPage);
